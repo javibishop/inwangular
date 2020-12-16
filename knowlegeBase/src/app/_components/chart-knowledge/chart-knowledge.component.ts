c375777bf839095e847e25f12943fc5d6e3cd4d6
Francisco Sanseverino <francisco.sanseverino@inworx.com>
2020-12-16 11:53:52 -0300

    subo cambios del charts

--- knowlegeBase/src/app/_components/chart-knowledge/chart-knowledge.component.ts
+++ knowlegeBase/src/app/_components/chart-knowledge/chart-knowledge.component.ts
@@ -1,48 +1,58 @@
-
-import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions, ChartType, Label } from 'chart.js';
-import { KnowledgeService } from 'src/app/service/knowledge.service';
-
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { UserService } from "src/app/service/user.service";
 
 @Component({
-  selector: 'app-chart-knowledge',
-  templateUrl: './chart-knowledge.component.html',
-  styleUrls: ['./chart-knowledge.component.css']
+  selector: "app-chart-knowledge",
+  templateUrl: "./chart-knowledge.component.html",
+  styleUrls: ["./chart-knowledge.component.css"],
 })
-export class ChartKnowledgeComponent implements OnInit {
-
-  public barChartOptions:any = {
+export class ChartKnowledgeComponent implements OnInit, OnDestroy {
+  public barChartOptions: any = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
   };
-  public barChartLabels:string[] = []//['Net.Core','Css','Angular','SQL','Oracle', 'c#'];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
- 
-  public barChartData:any[] = [];   
-   //{data: [8,2,3], label: 'Nivel'},   
- //];
-  listaConocimiento: any[];
+  public barChartLabels: string[] = []; //['Net.Core','Css','Angular','SQL','Oracle', 'c#'];
+  public barChartType: string = "bar";
+  public barChartLegend: boolean = true;
 
-  constructor(private knowledgeService: KnowledgeService) { }
+  public barChartData: any[] = [];
+  //{data: [8,2,3], label: 'Nivel'},
+  //];
+  listaConocimiento: any[];
+  subcription: Subscription;
+
+  constructor(private userService: UserService) {}
+
+  ngOnDestroy(): void {
+    this.subcription.unsubscribe();
+  }
 
   ngOnInit() {
-
-    this.knowledgeService.getById(JSON.parse(localStorage.getItem('currentUser')).id).subscribe((data: any) =>{
-      console.log(data.conocimientos);
-      this.listaConocimiento = data.conocimientos;
-      this.getLevelKnowledge()
-    })    
+    this.subcription = this.userService
+      .getConocimientos(JSON.parse(localStorage.getItem("currentUser")).id)
+      .subscribe((data: any) => {
+        this.listaConocimiento = data;
+        this.getLevelKnowledge();
+      });
   }
 
   getLevelKnowledge() {
-    const data= [];
-    this.listaConocimiento.forEach(x=> {
-      this.barChartLabels.push(x.conocimiento.nombre)
-      data.push(x.conocimiento.nivel)
+    const data = [];
+    this.listaConocimiento.forEach((x) => {
+      this.barChartLabels.push(x.conocimiento.nombre);
+      data.push(x.nivel);
     });
 
-    this.barChartData.push({data: data, label: 'Nivel'});
+    data.push(5);
+    this.barChartData.push({ data: data, label: "Nivel" });
   }
 
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
 }
